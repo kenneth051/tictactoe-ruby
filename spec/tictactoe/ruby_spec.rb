04c46eb24e5a5e1577b73f2515af6adb8b2ba54f
kenneth051 <kjdumba@gmail.com>
2020-03-03 01:31:49 +0200

    update tests

--- spec/tictactoe/ruby_spec.rb
+++ spec/tictactoe/ruby_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe Tictactoe::Ruby do
+  it "has a version number" do
+    expect(Tictactoe::Ruby::VERSION).not_to be nil
+  end
+
   it "should add 1 to 1" do
     expect(1+1).to eq(2)
   end
