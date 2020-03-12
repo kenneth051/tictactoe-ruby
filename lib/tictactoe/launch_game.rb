5248491736c298b335792278b4c05639ad1e12b3
kenneth051 <kjdumba@gmail.com>
2020-03-12 12:01:50 +0200

    update launch game tests and class

--- lib/tictactoe/launch_game.rb
+++ lib/tictactoe/launch_game.rb
@@ -1,7 +1,26 @@
+require 'json'
+
 module Tictactoe
     class LaunchGame
-        def start
-            puts "choose a language, choisissez une langue, 语言选择"
+        def initialize(json_file, stdin: $stdin)
+            @stdin=stdin
+            @obj=json_file 
+
         end
+
+        def choose_language()
+            while true
+                puts "Enter '1' for English, entrez 2 pour le français, 输入3作为普通话"
+                index =@stdin.gets.chomp
+                return @obj[index] if @obj[index] 
+            end
+        end
+
+        def start
+            language = choose_language()
+        end
+
+
+
     end
 end
--- spec/tictactoe/launch_game_spec.rb
+++ spec/tictactoe/launch_game_spec.rb
@@ -1,12 +1,28 @@
-
 require "./lib/tictactoe/launch_game"
 
 RSpec.describe Tictactoe::LaunchGame do
-  context "when user chooses a language" do
-    it "should lauch game" do
-      launch_game = Tictactoe::LaunchGame.new
-      launch_game.start
-      expect { launch_game.start }.to output("choose a language, choisissez une langue, 语言选择\n").to_stdout
+  context "#choose_language" do
+    it "should output a message to enable users choose a language" do
+      json=JSON.parse(File.read("./lib/tictactoe/settings.json"))
+      input = FakeStringInput.new(["1","1"])
+      launch_game = Tictactoe::LaunchGame.new(json, stdin:input)
+      launch_game.choose_language
+      expect { launch_game.choose_language }.to output("Enter '1' for English, entrez 2 pour le français, 输入3作为普通话\n").to_stdout
+    end
+    it "should return a user's language" do
+        json=JSON.parse(File.read("./lib/tictactoe/settings.json"))
+        input = FakeStringInput.new(["1","1"])
+        launch_game = Tictactoe::LaunchGame.new(json, stdin:input)
+        expect(launch_game.choose_language).to have_key("language") 
+      end
+  end
+  context "#start" do
+    it "should output a message to enable users choose a language" do
+      json=JSON.parse(File.read("./lib/tictactoe/settings.json"))
+      input = FakeStringInput.new(["1","3"])
+      launch_game = Tictactoe::LaunchGame.new(json, stdin:input)
+      expect { launch_game.start }.to output("Enter '1' for English, entrez 2 pour le français, 输入3作为普通话\n").to_stdout
     end
   end
+  
 end
